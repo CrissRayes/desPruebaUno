d5027abc0be12c59b4af70c9ebf357729452e879
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2023-01-26 11:53:23 -0300

    importa componente y pasa props

--- src/MiApi.jsx
+++ src/MiApi.jsx
@@ -1,3 +1,4 @@
+import Card from './components/Card'
 import useGetData from './hooks/useGetData'
 
 const MiApi = () => {
@@ -7,17 +8,12 @@ const MiApi = () => {
     <div>
       {loading && <p>Loading...</p>}
       <h1> Mi Api</h1>
-      <div>
-        {data.map(character => (
-          <div key={character.id}>
-            <p>{character.name}</p>
-            <img
-              src={character.src}
-              alt=''
-            />
-          </div>
-        ))}
-      </div>
+      {data.map(character => (
+        <Card
+          key={character.id}
+          {...character}
+        />
+      ))}
     </div>
   )
 }
