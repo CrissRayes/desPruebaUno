2c05a932cc87f064f8acb2a1f2ec28cd8b7af363
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2023-01-26 11:53:49 -0300

    aplica sort y agrega comentarios

--- src/helpers/getData.js
+++ src/helpers/getData.js
@@ -1,11 +1,18 @@
+// En la consulta a la API de marvel se han utilizado los siguientes parámetros:
+// orderBy=-modified: ordena los resultados por fecha de modificación, de forma descendente
+// limit=100: limita el número de resultados a 100
+// ts=1: es un parámetro requerido por la API para el timestamp
+// Se hizo de esta forma para luego aplicar el método sort() y ordenar los resultados por nombre
+
+
 const getData = async () => {
-  const url = 'https://gateway.marvel.com:443/v1/public/characters?limit=100&ts=1&apikey=9b47a875a6bc074de5b57f236d551413&hash=e7637815b05b0eb7de0568b7ff3e924f'
+  const url = 'https://gateway.marvel.com:443/v1/public/characters?orderBy=-modified&limit=100&ts=1&apikey=9b47a875a6bc074de5b57f236d551413&hash=e7637815b05b0eb7de0568b7ff3e924f'
   const response = await fetch( url )
   const data = await response.json()
   const { results } = data.data
-  console.log( results )
+  const resultsAscending = results.sort( ( a, b ) => a.name < b.name ? -1 : a.name > b.name ? 1 : 0 )
 
-  const items = results.map( item => (
+  const items = resultsAscending.map( item => (
     {
       id: item.id,
       name: item.name,
