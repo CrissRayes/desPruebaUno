256e15f608aa9e1f3b03ac8c52cdad349559c2e4
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2023-01-26 10:10:33 -0300

    recorre array de resultados

--- src/MiApi.jsx
+++ src/MiApi.jsx
@@ -1,7 +1,20 @@
+import useGetData from './hooks/useGetData'
+
 const MiApi = () => {
+  const { data, isLoading } = useGetData()
+
   return (
     <div>
       <h1> Mi Api</h1>
+
+      {isLoading && <p>Cargando...</p>}
+      <div>
+        {data.map(character => (
+          <div key={character.id}>
+            <p>{character.name}</p>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
--- src/helpers/getData.js
+++ src/helpers/getData.js
@@ -2,7 +2,18 @@ const getData = async () => {
   const url = 'https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=9b47a875a6bc074de5b57f236d551413&hash=e7637815b05b0eb7de0568b7ff3e924f'
   const response = await fetch( url )
   const data = await response.json()
-  console.log( data )
+  const { results } = data.data
+  console.log( results )
+
+  const items = results.map( item => (
+    {
+      id: item.id,
+      name: item.name,
+      description: item.description,
+      thumbnail: `${item.thumbnail.path}.${item.thumbnail.extension}`
+    } ) )
+  return items
+
 }
 
 export default getData
