e3ca6aa722fee1bb600f823e8eb3c9a8ab24c0fc
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2023-01-26 11:06:43 -0300

    actualiza consulta a api

--- src/helpers/getData.js
+++ src/helpers/getData.js
@@ -1,5 +1,5 @@
 const getData = async () => {
-  const url = 'https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=9b47a875a6bc074de5b57f236d551413&hash=e7637815b05b0eb7de0568b7ff3e924f'
+  const url = 'https://gateway.marvel.com:443/v1/public/characters?limit=100&ts=1&apikey=9b47a875a6bc074de5b57f236d551413&hash=e7637815b05b0eb7de0568b7ff3e924f'
   const response = await fetch( url )
   const data = await response.json()
   const { results } = data.data
@@ -10,7 +10,7 @@ const getData = async () => {
       id: item.id,
       name: item.name,
       description: item.description,
-      thumbnail: `${item.thumbnail.path}.${item.thumbnail.extension}`
+      src: `${item.thumbnail.path}.${item.thumbnail.extension}`
     } ) )
   return items
 
